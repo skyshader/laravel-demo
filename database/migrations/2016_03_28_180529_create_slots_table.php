26b1680f3dfb7c8c3e38f24d294d141d6787229a
Shardendu Kumar Gautam <shardendukumargautam@gmail.com>
2016-03-28 23:37:02 +0530

    Added slot migration and model'

---
+++ database/migrations/2016_03_28_180529_create_slots_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->tinyInteger('day');
+            $table->time('slot_from');
+            $table->time('slot_to');
+            $table->integer('academy_id')->unsigned()->index();
+            $table->tinyInteger('status')->default(1);
+
+            $table->timestamps();
+
+            $table->foreign('academy_id')
+                  ->references('id')->on('academies')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('slots', function ($table) {
+        
+                $table->dropForeign(['academy_id']);
+        });
+        Schema::drop('slots');
+    }
+}
