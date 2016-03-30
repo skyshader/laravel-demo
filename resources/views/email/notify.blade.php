3dd3a2eb132a0b4d59b54ee54a655c26c52f9b5a
Shardendu Kumar Gautam <shardendukumargautam@gmail.com>
2016-03-30 16:52:43 +0530

    color to header

--- resources/views/email/notify.blade.php
+++ resources/views/email/notify.blade.php
@@ -8,7 +8,7 @@
 <body style="font-family: Helvetica, Arial, Verdana, Trebuchet MS; background: #f9fafb; margin: 0;">
 	<div class="container" style="width: 500px; max-width:500px; margin: 25px auto; background: #fff; border: 1px solid #f4f6f8; border-radius: 3px; box-sizing: border-box;">
 		<div class="header" style="text-align: center; background: #41245C; color: #fff; padding: 20px; border-radius: 3px 3px 0 0; border-bottom: 1px solid #1a0e25; box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.7);">
-			<h1 style="">academia</h1>
+			<h1 style="color: #fff;">academia</h1>
 		</div>
 		<div class="content" style="padding: 25px; padding-bottom: 0;">
 			<h1>Hey admin,</h1>
